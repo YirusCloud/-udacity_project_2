aff5a0c3d08f97a7f4047d33f505345980148933
YirusCloud <84454715+YirusCloud@users.noreply.github.com>
2023-03-20 20:33:46 +0800

    Update problem_5.py

--- P1/problem_5.py
+++ P1/problem_5.py
@@ -1,44 +1,60 @@
-import hashlib
+
+
 import datetime
+import hashlib
+
 
 class Block:
+
     def __init__(self, timestamp, data, previous_hash):
-      self.timestamp = timestamp
-      self.data = data
-      self.previous_hash = previous_hash
-      self.hash = self.calc_hash()
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calc_hash()
 
     def calc_hash(self):
-      sha = hashlib.sha256()
+        sha = hashlib.sha256()
 
-      hash_str = "We are going to encode this string of data!".encode('utf-8')
+        hash_str = "We are going to encode this string of data!".encode('utf-8')
 
-      sha.update(hash_str)
+        sha.update(hash_str)
 
-      return sha.hexdigest()
+        return sha.hexdigest()
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.last = None
 
-    def append(self, timestamp, data, previous_hash=0):
-        new_node = Block(timestamp, data, previous_hash)
-        if self.head is None:
-            self.head = new_node
+    def append(self, timestamp, data):
+        if not self.head:
+            self.head = Block(timestamp, data, 0)
+            self.last = self.head
         else:
-            new_node.previous_hash = self.head
-            self.head = new_node
-            
+            temp_data = self.last
+            self.last = Block(timestamp, data, temp_data)
+            self.last.previous_hash = temp_data
 
 
 def get_timestamp():
-    return datetime.datetime.utcnow().strftime("%H:%M:%S")
+    return datetime.datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S")
 
-first = Block(get_timestamp(), "Hello", 0)
-second = Block(get_timestamp(), "World", first)
-third = Block(get_timestamp(), "Start", second)
 
+# blocks
+block_zero = Block(get_timestamp(), "Information X", 0)
+block_one = Block(get_timestamp(), "Information Y", block_zero)
+block_two = Block(get_timestamp(), "Information Z", block_one)
+
+# linked list
 linked_list = LinkedList()
-linked_list.append(first)
-linked_list.append(second)
+linked_list.append(get_timestamp(), "Information A")
+linked_list.append(get_timestamp(), "Information B")
+
+# tests
+print("Block Zero data : ", block_zero.data)
+print("Block Zero hash : ", block_zero.hash)
+print("Block Zero timestamp : ", block_zero.timestamp)
+print("Block one's previous block's data : ", block_one.previous_hash.data)
+print("Linked list last data : ", linked_list.last.data)
+print("Linked list last's previous hash data : ", linked_list.last.previous_hash.data)
